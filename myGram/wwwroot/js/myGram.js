e6209b45d2705c044d14231f07f476ea03ae4130
Jonathan Marquez <jmarqu12@icloud.com>
2021-04-03 01:04:03 -0600

    function

--- myGram/wwwroot/js/myGram.js
+++ myGram/wwwroot/js/myGram.js
@@ -1,17 +1,13 @@
-﻿
-var mySpan = document.getElementById("myGram");
-var newSpan = document.createElement("span");
-newSpan, innerHTML = "My Gram";
-mySpan.appendChild(newSpan);
-var imageDiv = document.getElementsByClassName("image");
-var newSpan0 = document.createElement("span0");
-newSpan0.innerHTML = "Image 1 Image 2 Image 3 Image 4 Image 5";
-imageDiv.appendChild(newSpan0);
-var myDiv1 = document.getElementsByClassName("mySpan");
-var newSpan1 = document.createElement("span1");
-newSpan1.innerHTML = "About Blog Jobs Help";
-myDiv1.appendChild(newSpan1);
-var myDiv2 = document.getElementsByClassName("mySpan2");
-var newSpan2 = document.createElement("span2");
-newSpan2.innerHTML = "English 2021 MyGram";
-myDiv2.appendChild(newSpan2);
+﻿var a = 1000000;
+var b = 10;
+var c = a % b;
+
+function jonathan() {
+    console.log(c);
+    console.log("Learning Javascript Is Fun");
+}
+
+jonathan();
+
+
+
