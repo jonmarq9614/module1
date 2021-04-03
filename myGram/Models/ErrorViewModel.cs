315ce0897237711f1243a20c88bc562f77546afb
Jonathan Marquez <jmarqu12@icloud.com>
2021-04-03 00:37:49 -0600

    myGram assignment

---
+++ myGram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myGram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
